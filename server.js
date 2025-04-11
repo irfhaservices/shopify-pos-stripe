94987cac5e84245abd122577e523a23056ba42d0
irfhaservices <130183625+irfhaservices@users.noreply.github.com>
2025-04-11 15:53:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')('sk_test_dummy');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post('/api/stripe/connection_token', async (req, res) => {
-  const connectionToken = await stripe.terminal.connectionTokens.create();
-  res.json({ secret: connectionToken.secret });
+  try {
+    const connectionToken = await stripe.terminal.connectionTokens.create();
+    res.json({ secret: connectionToken.secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.post('/api/stripe/create_payment_intent', async (req, res) => {
-  const { amount } = req.body;
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount,
-    currency: 'usd',
-    payment_method_types: ['card_present'],
-    capture_method: 'manual',
-  });
-  res.json({ client_secret: paymentIntent.client_secret });
+  try {
+    const { amount } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      payment_method_types: ['card_present'],
+      capture_method: 'automatic'
+    });
+    res.json({ client_secret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.get('/', (req, res) => {
